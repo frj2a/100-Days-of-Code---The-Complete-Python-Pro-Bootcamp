f74569a8582abcc3ca72b35254a251ecc1d354b9
frj2a <francisco@aivot.com>
2025-05-02 20:49:58 -0300

    up to day 17, last part

--- Day 17/quiz-game-start/main.py
+++ Day 17/quiz-game-start/main.py
@@ -4,7 +4,7 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for data in question_data:
-    question_bank.append(Question(data["text"], data["answer"]))
+    question_bank.append(Question(data["question"], data["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 
