4099cbaf3fe44dfe2fff9ffbf22dd023d8c27c84
frj2a <francisco@aivot.com>
2025-05-12 18:25:08 -0300

    up to day 24

--- Day 24/mail-merge-project/main.py
+++ Day 24/mail-merge-project/main.py
@@ -1,4 +1,8 @@
-#TODO: Create a letter using starting_letter.txt 
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+#TODO: Create a letter using starting_letter.txt
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
@@ -6,3 +10,14 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+    # print(letter)
+    with open("Input/Names/invited_names.txt") as names_file:
+        names = names_file.readlines()
+        for name in names:
+            new_letter = letter.replace("[name]", name.strip())
+            print(new_letter)
+            with open(f"Output/ReadyToSend/letter_for_{name}.txt","w") as final_letter_file:
+                final_letter_file.write(new_letter)
